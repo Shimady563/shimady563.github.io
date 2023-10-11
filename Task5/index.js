7f9e9045d817c5b936a68fe96e159f9719f4a063
Maksim Shiryaev <shimady563@gmail.com>
2023-10-11 21:44:29 +0300

    1

--- Task5/index.js
+++ Task5/index.js
@@ -1,18 +1,22 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-    let calculateButton = document.getElementById('calculateButton');
-    calculateButton.addEventListener('click', function (event) {
+/*global document */
+
+let doc = document;
+doc.addEventListener("DOMContentLoaded", function () {
+    let calculateButton = doc.getElementById("calculateButton");
+    calculateButton.addEventListener("click", function (event) {
         event.preventDefault();
-        let quantityInput = document.getElementById('quantity');
-        let productSelect = document.getElementById('product');
-        let result = document.getElementById('result');
+        let quantityInput = doc.getElementById("quantity");
+        let productSelect = doc.getElementById("product");
+        let result = doc.getElementById("result");
 
         let str1 = quantityInput.value;
         let str2 = productSelect.value;
         if (str1.match(/^\d+$/) === null || str2.match(/^\d+$/) === null) {
             result.innerHTML = "Ошибка, некорректные данные";
-        }
-        else {
-            let ans = parseInt(quantityInput.value) * parseInt(productSelect.value); 
+        } else {
+            let temp1 = parseInt(quantityInput.value);
+            let temp2 = parseInt(productSelect.value);
+            let ans = temp1 * temp2;
             result.innerHTML = "Стоимость заказа: $" + ans;
         }
     });
