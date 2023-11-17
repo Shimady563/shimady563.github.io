814a994c0a2ea79328db4be0ce76cb04ef79b8fe
Maksim Shiryaev <shimady563@gmail.com>
2023-11-17 23:21:49 +0300

    1

--- Task8/index.js
+++ Task8/index.js
@@ -67,10 +67,12 @@ document.addEventListener("DOMContentLoaded", function () {
         history.pushState({page: "form"}, "Форма обратной связи", "?form=true");
     });
 
-    window.addEventListener("popstate", function () {
-        popupDiv.classList.add("hidden");
-        messageDiv.innerHTML = "";
-        messageDiv.classList.add("hidden");
+    window.addEventListener("popstate", function (event) {
+        if (event.state && event.state.page === "form") {
+            popupDiv.classList.add("hidden");
+            messageDiv.innerHTML = "";
+            messageDiv.classList.add("hidden");
+        }
     });
 
     let submitForm = document.getElementById("form-to-submit");
